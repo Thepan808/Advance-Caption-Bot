277a896494c6177cbb7f1a1476b5136dc22d734f
♔•アરเέຮ┼•♔ ぶ <streetdemonic003@gmail.com>
2025-01-24 18:52:28 -0300

    Update info.py

--- info.py
+++ info.py
@@ -14,13 +14,13 @@ def is_enabled(value, default):
         return default
 
 
-ADMIN = int(getenv("ADMIN", ""))
+ADMIN = int(getenv("ADMIN", "737737727"))
 SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = str(getenv("API_HASH", ""))
-BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
+API_ID = int(getenv("API_ID", "4954361"))
+API_HASH = str(getenv("API_HASH", "43a786a8548a30f9d6887e36d53c0e64"))
+BOT_TOKEN = str(getenv("BOT_TOKEN", "7661519198:AAHooIqTUBl-FJI07hpg3Ui2PrnN9rGGCw4"))
 FORCE_SUB = os.environ.get("FORCE_SUB", "") 
-MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://replacewithyourmongodb:replacewithyourmongodb@cluster0.zi78j51.mongodb.net/?retryWrites=true&w=majority",))
+MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://thepanda:thepanda@cluster0.4kfqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
     getenv(
         "DEF_CAP",
